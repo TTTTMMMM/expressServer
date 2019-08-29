6f99cdc611f6154d5e8597624b4b4560f2903625
TTTTMMMM <junque135@gmail.com>
2019-08-29 19:57:30 -0400

    Got rid of IP address, since Google Cloud Platform (GCP) errors out on it; GCP sets the host, not you.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+/* Tutorial: https://www.youtube.com/watch?v=L72fhGm1tfE */
 
 const server = express();
 
@@ -8,4 +9,4 @@ server.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 5001;
 
-server.listen(PORT, '192.168.1.151', () => console.log(`Express server started on port ${PORT}`));
+server.listen(PORT, () => console.log(`Express server started on port ${PORT}`));
