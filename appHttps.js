6e092488192622d8a28e8c076ec1fe4c93bf42e1
TTTTMMMM <junque135@gmail.com>
2019-09-01 11:13:23 -0400

    Can start either the unsecure or the secure server

--- appHttps.js
+++ appHttps.js
@@ -13,15 +13,15 @@ const serverHttps = express();
 
 serverHttps.use(express.static(path.join(__dirname, 'public')));  // Set a static folder
 
-/*
-const PORT = process.env.PORT || 5001;
 
+const PORT = process.env.PORT || 5001;
+/*
 serverHttps.listen(PORT, () => console.log(`Express https server started on port ${PORT}`));
  */
 https.createServer({
     key: fs.readFileSync('/Users/tm/devDir/security/antonios-iMac.fios-router.home_key.pem'),
     cert: fs.readFileSync('/Users/tm/devDir/security/antonios-iMac.fios-router.home.cer.pem')
   }, serverHttps)
-  .listen(5001, function () {
-    console.log('Example https server listening on port 5001! Go to https://localhost:5001/')
+  .listen(PORT, function () {
+    console.log(`Https server listening on port ${PORT} Go to https://localhost:${PORT}/`)
   })
